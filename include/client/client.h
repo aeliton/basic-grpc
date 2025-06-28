b8d93ab9fe5b8ff54878c719e7cce66269c67ec9
Aeliton G. Silva <aeliton@gmail.com>
2025-06-28 14:08:03 -0300

    Move client code to a class
    Also makes the client program to read input from the user in a loop.

--- include/client/client.h
+++ include/client/client.h
@@ -4,4 +4,16 @@
 #include <proto/example.grpc.pb.h>
 #include <proto/example.pb.h>
 
+class CharClient
+{
+public:
+  explicit CharClient(const std::string& location);
+  grpc::Status count(const std::string& text, uint32_t& count);
+
+private:
+  std::string _location;
+  std::shared_ptr<grpc::Channel> _channel;
+  grpc::Status _status;
+};
+
 #endif
--- src/client.cpp
+++ src/client.cpp
@@ -5,26 +5,48 @@
 
 #include <iostream>
 
-int main()
+CharClient::CharClient(const std::string& location)
+  : _location(location)
+  , _channel(grpc::CreateChannel(_location, grpc::InsecureChannelCredentials()))
+  , _status(grpc::Status::OK)
 {
+}
+
+grpc::Status CharClient::count(const std::string& text, uint32_t& count)
+{
+  auto stub = example::Char::NewStub(_channel);
+  grpc::ClientContext context;
   example::Text input;
   example::Number output;
-
-  input.set_content("abaco");
-
-  auto channel =
-    grpc::CreateChannel("localhost:50001", grpc::InsecureChannelCredentials());
-  std::unique_ptr<example::Char::Stub> stub = example::Char::NewStub(channel);
-  grpc::ClientContext context;
-  grpc::Status status = stub->Count(&context, input, &output);
-
+  input.set_content(text);
+  auto status = stub->Count(&context, input, &output);
   if (status.ok()) {
-    std::cout << "Client: char count of [" << input.content()
-              << "] is:" << output.content() << std::endl;
-  } else {
-    std::cout << "Client: Error: the requesst has failed with error code: "
-              << status.error_code() << std::endl;
+    count = output.content();
   }
+  return status;
+}
 
+int main()
+{
+  std::string input;
+  CharClient client("localhost:50001");
+
+  while (true) {
+    std::cout << "(q: exit) > ";
+    std::cin >> input;
+    if (input == "q") {
+      break;
+    }
+
+    uint32_t count;
+    auto status = client.count(input, count);
+    if (status.ok()) {
+      std::cout << "Client: char count of [" << input << "] is:" << count
+                << std::endl;
+    } else {
+      std::cerr << "Client: Error: the requesst has failed with error code: "
+                << status.error_code() << std::endl;
+    }
+  }
   return 0;
 }
